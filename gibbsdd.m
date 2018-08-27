05465c24f4a7fe378171a843454b5bf7de0ba4a7
VISHAL Subramanian <vishalsubbu97@gmail.com>
2018-08-27 13:14:48 +0530

    Update gibbsdd.m

--- gibbsdd.m
+++ gibbsdd.m
@@ -1,1 +1,13 @@
-%% Copyright (c) 2018, Vishal_S% All rights reserved. Please read the "license.txt" for license terms.%% Project Title: Computational Materials Thermodynamics% % Developer: Vishal S% % Contact Info: vishalsubbu97@gmail.com%function y = gibbsdd(x,a)   y = -2*a + 1./x + 1./(1-x);endfunction
+%
+% Copyright (c) 2018, Vishal_S
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Title: Phase field modelling
+% 
+% Developer: Vishal S
+% 
+% Contact Info: vishalsubbu97@gmail.com
+%
+function y = gibbsdd(x,a) 
+  y = -2*a + 1./x + 1./(1-x);
+endfunction
