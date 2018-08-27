5731d2d147781ddeacd9d025b20741b7dc0db7cb
VISHAL Subramanian <vishalsubbu97@gmail.com>
2018-08-27 13:20:07 +0530

    Update gibbs.m

--- gibbs.m
+++ gibbs.m
@@ -1,1 +1,13 @@
-%% Copyright (c) 2018, Vishal_S% All rights reserved. Please read the "license.txt" for license terms.%% Project Title: Computational Materials Thermodynamics% % Developer: Vishal S% % Contact Info: vishalsubbu97@gmail.com%function y = gibbs(x,a)   y = a.*x.*(1-x) + x.*log(x) + (1-x).*(log(1-x));endfunction
+%
+% Copyright (c) 2018, Vishal_S
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Title: Phase field modelling
+% 
+% Developer: Vishal S
+% 
+% Contact Info: vishalsubbu97@gmail.com
+%
+function y = gibbs(x,a) 
+  y = a.*x.*(1-x) + x.*log(x) + (1-x).*(log(1-x));
+endfunction
